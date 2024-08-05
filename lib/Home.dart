a8212de667c7970c2ba65876701847a4b1e76c5b
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-08-05 16:08:26 +0500

    recorder app

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
@@ -41,7 +43,7 @@ class _MyWidgetState extends State<Home> {
     }
   }
 
-  void startTimer() {
+  void _startTimer() {
     const oneSec = Duration(seconds: 1);
     _timer = Timer.periodic(oneSec, (Timer timer) {
       setState(() {
@@ -88,6 +90,130 @@ class _MyWidgetState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: ListView(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          SizedBox(
+            height: 500,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: IconButton(
+                    icon: Image.asset("images/logo.png"),
+                    onPressed: () {
+                      if (_isrecording) {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 150,
+                                    width: 350,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          margin: EdgeInsets.all(20),
+                                          height: 50,
+                                          child: Material(
+                                            child: TextField(
+                                              controller: _controller,
+                                              textAlignVertical:
+                                                  TextAlignVertical.center,
+                                              decoration: const InputDecoration(
+                                                  isDense: true,
+                                                  fillColor: Colors.white,
+                                                  border: OutlineInputBorder(),
+                                                  contentPadding:
+                                                      EdgeInsets.all(12)),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 20, right: 20),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        GestureDetector(
+                                          onTap: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: Container(
+                                            height: 40,
+                                            width: 80,
+                                            color: Colors.blue,
+                                            alignment: Alignment.center,
+                                            child: const Text(
+                                              "Cancel",
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.white,
+                                                  decoration:
+                                                      TextDecoration.none),
+                                            ),
+                                          ),
+                                        ),
+                                        GestureDetector(
+                                          onTap: () {
+                                            Navigator.pop(context);
+                                            if (_controller.text.isNotEmpty) {
+                                              _start();
+                                              _startTimer();
+                                              setState(() {
+                                                _isrecording = false;
+                                              });
+                                            }
+                                          },
+                                          child: Container(
+                                            height: 40,
+                                            width: 80,
+                                            color: Colors.blue,
+                                            alignment: Alignment.center,
+                                            child: const Text(
+                                              "Save",
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.white,
+                                                  decoration:
+                                                      TextDecoration.none),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              );
+                            });
+                      } else {
+                        _stop();
+                        _timer?.cancel();
+                        setState(() {
+                          _isrecording = true;
+                          _time = 0;
+                        });
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
